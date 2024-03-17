e74919afb17e58c9f6f0f28be356ab231e143079
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2024-03-17 16:00:15 +0530

    feat: setting layout of our app

---
+++ app/(marketing)/Footer.tsx
@@ -0,0 +1,5 @@
+export const Footer = () => {
+  return (
+    <footer className="hidden w-full h-20 lg:block border-t-2 border-slate-200 px-4"></footer>
+  );
+};
---
+++ app/(marketing)/Header.tsx
@@ -0,0 +1,5 @@
+export const Header = () => {
+  return (
+    <header className="w-full h-20 border-b-2 border-slate-200 px-4"></header>
+  );
+};
