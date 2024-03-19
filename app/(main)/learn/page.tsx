cbc7a535997bf2cb40deb50f50e4ddf83e6812df
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2024-03-19 18:31:30 +0530

    feat: creating learning page layout

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import { Header } from './header';
+import { FeedWrapper } from '@/components/feed-wrapper';
+import { StickyWrapper } from '@/components/sticky-wrapper';
+import { UserProgress } from '@/components/user-progress';
 
 const LearnPage = () => {
-  return <div>LearnPage</div>;
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: 'English', imageSrc: '/flag/us.svg' }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="English" />
+      </FeedWrapper>
+    </div>
+  );
 };
 
 export default LearnPage;
